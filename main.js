90865733da262698d164c683db5b47cee05676f5
Muhammad rivaldo <102275708+Muhammadrivaldo191@users.noreply.github.com>
2022-05-20 23:15:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,46 +1,36 @@
 Vue.createApp({
-    data() {
-      return {
-        header: {
-          title: "Muhammad Rivaldo",
-          description: "Kerja keras apa pun hasilnya jangan lupa untuk tetap bersyukur",
-          imageProfile: "https://github.com/Muhammadrivaldo191/Tekweb2022/blob/main/img/image-1.png",
-          social: {
-            ig: {
-              url: "https://instagram.com/muhammadrivaldo._",
-              title: "Muhammad Rivaldo",
-            },
-            fb: {
-              url: "https://web.facebook.com/Muhammad rivaldo/",
-              title: "Muhammad Rivaldo",
-            },
-            github: {
-              url: "https://github.com/Muhammadrivaldo191/",
-              title: "Muhammad Rivaldo",
-            },
-          },
-
-        },
-        articles1: 
-          {
-            title: 'Kabar Futsal Indonesia',
-            description: "kabar terbaru tentang futsal idnonesia.e",
-            thumbnail: 'img/image-5.jpg',
-          },
-        
-        articles2: 
-          {
-            title: 'Tips Menambah Stamina',
-            description: "Menjaga kebugaran tubuh menjadi kunci untuk meningkatkan stamina serta performa Anda di lapangan.",
-            thumbnail: 'img/image-6.jpg',
-          },
-        
-          articles3: 
-          {
-            title: 'Imunitas Saat Pandemi',
-            description: "Pola hidup sehat merupakan gaya hidup yang memperhatikan segala aspek kesehatan.",
-            thumbnail: 'img/image-2.jpg',
-          },
-      };
+  data() {
+    return {
+      header: {},
+      articles: [],
+    };
+  },
+  methods: {
+    getHeaderData() {
+      axios
+        .get('https://raw.githubusercontent.com/Muhammadrivaldo191/tekweb2022/main/contens/header.json')
+        .then((res) => {
+          console.log(res.data);
+          this.header = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-  }).mount("#app");
+    getArticlesData() {
+      axios
+        .get('https://raw.githubusercontent.com/Muhammadrivaldo191/tekweb2022/main/contens/articles.json')
+        .then((res) => {
+          console.log(res.data);
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  beforeMount() {
+    this.getHeaderData();
+    this.getArticlesData();
+  },
+}).mount('#app');
