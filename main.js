c056e78e821b68ea143756c06b19653d9a561b54
Muhammad rivaldo <102275708+Muhammadrivaldo191@users.noreply.github.com>
2022-05-21 01:22:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ Vue.createApp({
   methods: {
     getHeaderData() {
       axios
-        .get('https://raw.githubusercontent.com/Muhammadrivaldo191/tekweb2022/main/content/header.json')
+        .get('https://raw.githubusercontent.com/Muhammadrivaldo191/Tekweb2022/main/content/header.json')
         .then((res) => {
           console.log(res.data);
           this.header = res.data;
@@ -19,7 +19,7 @@ Vue.createApp({
     },
     getArticlesData() {
       axios
-        .get('https://raw.githubusercontent.com/Muhammadrivaldo191/tekweb2022/main/content/articles.json')
+        .get('https://raw.githubusercontent.com/Muhammadrivaldo191/Tekweb2022/main/content/articles.json')
         .then((res) => {
           console.log(res.data);
           this.articles = res.data;
